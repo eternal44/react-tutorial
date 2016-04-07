01e0cf072687a671b91606e3f281344a047034c9
James Youn <jamesyoun710@gmail.com>
2016-04-06 23:29:18 -0700

    Add search bar function component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDytR0Dz4b-fX6MsfHIpC_v7PQeqaJh2m0';
 
 // Create a new component.  This component should produce some html
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it in the DOM
