19afa80f32203383531618e5f2747b787288f017
James Youn <jamesyoun710@gmail.com>
2016-04-08 11:27:00 -0700

    Add selected video feature (see notes)
    This is a great example of an event emitter function altering the state
    of a component

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 // instead of passing in 'props' as an arg and then accessing 
 // props.video we can use {video}'
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
